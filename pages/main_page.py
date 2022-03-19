54c3dff08ace108c8f909c99aed7f60e57fb7ed1
Ready1337 <egor.zinovjev2013@yandex.ru>
2022-03-19 21:35:29 +0500

    MainPageLocators moved to locators

--- pages/main_page.py
+++ pages/main_page.py
@@ -5,9 +5,11 @@ from pages.locators import MainPageLocators
 
 
 class MainPage(BasePage):
+    locators = MainPageLocators()
+
     def go_to_login_page(self):
-        login_link = self.browser.find_element(*MainPageLocators.LOGIN_LINK)
+        login_link = self.browser.find_element(*self.locators.LOGIN_LINK)
         login_link.click()
 
     def should_be_login_link(self):
-        assert self.is_element_present(*MainPageLocators.LOGIN_LINK), "Login link is not presented"
+        assert self.is_element_present(*self.locators.LOGIN_LINK), "Login link is not presented"
