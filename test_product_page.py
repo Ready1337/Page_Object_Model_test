e505c9e1d09d081ca03ca9a277f7777d1aace7ac
Ready1337 <egor.zinovjev2013@yandex.ru>
2022-03-21 01:56:36 +0500

    basket tests grouped up to the following class

--- test_product_page.py
+++ test_product_page.py
@@ -1,46 +1,50 @@
 import pytest
+import time
 
 from pages.product_page import ProductPage
 from pages.login_page import LoginPage
 from pages.basket_page import BasketPage
 
 
-@pytest.mark.skip('skip')
-@pytest.mark.parametrize(
-    "url_suffix",
-    [
-        pytest.param("?promo=offer7", marks=pytest.mark.xfail) if i == 7 else f"?promo=offer{i}" for i in range(10)
-    ]
-)
-def test_guest_can_add_product_to_basket(browser, url_suffix):
-    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
-    product_page = ProductPage(browser, link + url_suffix)
-    product_page.open()
-    product_page.click_add_to_basket()
-    product_page.solve_quiz_and_get_code()
-    product_page.should_be_success_message()
+class TestUserAddToBasketFromProductPage:
+    @pytest.fixture(scope='function', autouse=True)
+    def setup(self, browser):
+        link = 'http://selenium1py.pythonanywhere.com/accounts/login/'
+        login_page = LoginPage(browser, link)
+        login_page.open()
+        email = str(time.time()) + '@fakemail.org'
+        password = str(time.time()) + '1234567890'
+        login_page.register_new_user(email, password)
+        login_page.should_be_authorized_user()
+
+    @pytest.mark.skip('skip')
+    def test_user_cant_see_success_message(self, browser):
+        link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
+        product_page = ProductPage(browser, link)
+        product_page.open()
+        product_page.should_not_be_success_message()
+
+    @pytest.mark.skip('skip')
+    def test_user_can_add_product_to_basket(self, browser):
+        link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
+        product_page = ProductPage(browser, link)
+        product_page.open()
+        product_page.click_add_to_basket()
+        product_page.should_be_success_message()
 
 
 @pytest.mark.skip('skip')
 def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
-    link = "http://selenium1py.pythonanywhere.com/ru/catalogue/coders-at-work_207/"
+    link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
     product_page = ProductPage(browser, link)
     product_page.open()
     product_page.click_add_to_basket()
-    product_page.should_not_be_success_message()
-
-
-@pytest.mark.skip('skip')
-def test_guest_cant_see_success_message(browser):
-    link = "http://selenium1py.pythonanywhere.com/ru/catalogue/coders-at-work_207/"
-    product_page = ProductPage(browser, link)
-    product_page.open()
     product_page.should_not_be_success_message()
 
 
 @pytest.mark.skip('skip')
 def test_message_disappeared_after_adding_product_to_basket(browser):
-    link = "http://selenium1py.pythonanywhere.com/ru/catalogue/coders-at-work_207/"
+    link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
     product_page = ProductPage(browser, link)
     product_page.open()
     product_page.click_add_to_basket()
@@ -49,7 +53,7 @@ def test_message_disappeared_after_adding_product_to_basket(browser):
 
 @pytest.mark.skip('skip')
 def test_guest_should_see_login_link_on_product_page(browser):
-    link = 'http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/'
+    link = 'http://selenium1py.pythonanywhere.com/catalogue/the-city-and-the-stars_95/'
     product_page = ProductPage(browser, link)
     product_page.open()
     product_page.should_be_login_link()
@@ -57,7 +61,7 @@ def test_guest_should_see_login_link_on_product_page(browser):
 
 @pytest.mark.skip('skip')
 def test_guest_can_go_to_login_page_from_product_page(browser):
-    link = 'http://selenium1py.pythonanywhere.com/ru/catalogue/coders-at-work_207/'
+    link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
     product_page = ProductPage(browser, link)
     product_page.open()
     product_page.go_to_login_page()
@@ -65,8 +69,9 @@ def test_guest_can_go_to_login_page_from_product_page(browser):
     login_page.should_be_login_page()
 
 
+@pytest.mark.skip('skip')
 def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
-    link = 'http://selenium1py.pythonanywhere.com/ru/catalogue/coders-at-work_207/'
+    link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
     products_page = ProductPage(browser, link)
     products_page.open()
     products_page.go_to_basket_page()
