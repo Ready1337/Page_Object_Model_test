bd0dd11b9cc9ceac084ad898020f064105733ebe
Ready1337 <egor.zinovjev2013@yandex.ru>
2022-03-20 18:19:11 +0500

    added methods for negative tests

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,22 +1,41 @@
-from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import NoSuchElementException, TimeoutException
 from selenium.common.exceptions import NoAlertPresentException
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 import math
 
 
 class BasePage:
-    def __init__(self, browser, url, timeout=10):
+    def __init__(self, browser, url):
         self.browser = browser
         self.url = url
-        self.browser.implicitly_wait(timeout)
 
     def open(self):
         self.browser.get(self.url)
 
-    def is_element_present(self, how, what):
+    def is_element_present(self, locator):
         try:
-            self.browser.find_element(how, what)
+            self.browser.find_element(*locator)
         except NoSuchElementException:
             return False
+        return True
+
+    def is_not_element_present(self, locator, timeout=4):
+        try:
+            WebDriverWait(self.browser, timeout).\
+                until(EC.presence_of_element_located(locator))
+        except TimeoutException:
+            return True
+
+        return False
+
+    def is_disappeared(self, locator, timeout=4):
+        try:
+            WebDriverWait(self.browser, timeout, 1, TimeoutException).\
+                until_not(EC.presence_of_element_located(locator))
+        except TimeoutException:
+            return False
+
         return True
 
     def solve_quiz_and_get_code(self):
