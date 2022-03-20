00e5ade4c504c15d601561766804fc097648a7ac
Ready1337 <egor.zinovjev2013@yandex.ru>
2022-03-20 15:51:21 +0500

    test_guest_can_add_product_to_basket parametrized

--- test_product_page.py
+++ test_product_page.py
@@ -1,9 +1,18 @@
+import pytest
+
 from pages.product_page import ProductPage
 
 
-def test_guest_can_add_product_to_basket(browser):
-    link = "http://selenium1py.pythonanywhere.com/ru/catalogue/the-shellcoders-handbook_209/?promo=newYear"
-    product_page = ProductPage(browser, link)
+@pytest.mark.parametrize(
+    "url_suffix",
+    [
+        f"?promo=offer{i}" for i in range(10)
+    ]
+)
+def test_guest_can_add_product_to_basket(browser, url_suffix):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+    product_page = ProductPage(browser, link + url_suffix)
     product_page.open()
     product_page.click_add_to_basket()
     product_page.solve_quiz_and_get_code()
+    product_page.should_be_success_message()
