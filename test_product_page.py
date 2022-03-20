558295a070202e6d7f2a403d1311def37764cc56
Ready1337 <egor.zinovjev2013@yandex.ru>
2022-03-20 18:18:05 +0500

    negative tests added

--- test_product_page.py
+++ test_product_page.py
@@ -3,6 +3,7 @@ import pytest
 from pages.product_page import ProductPage
 
 
+@pytest.mark.skip('skip')
 @pytest.mark.parametrize(
     "url_suffix",
     [
@@ -16,3 +17,28 @@ def test_guest_can_add_product_to_basket(browser, url_suffix):
     product_page.click_add_to_basket()
     product_page.solve_quiz_and_get_code()
     product_page.should_be_success_message()
+
+
+@pytest.mark.skip('skip')
+def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
+    link = "http://selenium1py.pythonanywhere.com/ru/catalogue/coders-at-work_207/"
+    product_page = ProductPage(browser, link)
+    product_page.open()
+    product_page.click_add_to_basket()
+    product_page.should_not_be_success_message()
+
+
+def test_guest_cant_see_success_message(browser):
+    link = "http://selenium1py.pythonanywhere.com/ru/catalogue/coders-at-work_207/"
+    product_page = ProductPage(browser, link)
+    product_page.open()
+    product_page.should_not_be_success_message()
+
+
+@pytest.mark.skip('skip')
+def test_message_disappeared_after_adding_product_to_basket(browser):
+    link = "http://selenium1py.pythonanywhere.com/ru/catalogue/coders-at-work_207/"
+    product_page = ProductPage(browser, link)
+    product_page.open()
+    product_page.click_add_to_basket()
+    product_page.should_not_be_success_message_with_disappeared()
