f7b3f3ffa63dc48ec78bb0212233f43dd4e88699
Ready1337 <egor.zinovjev2013@yandex.ru>
2022-03-20 23:20:40 +0500

    quotations marks changed

--- pages/product_page.py
+++ pages/product_page.py
@@ -25,13 +25,13 @@ class ProductPage(BasePage):
         alert_product_name = self.get_alert_product_name()
         product_name = self.get_product_name()
         assert alert_product_name == product_name, \
-            f"{product_name} is not presented in success message: {alert_product_name} instead"
+            f'{product_name} is not presented in success message: {alert_product_name} instead'
 
     def should_be_product_price_in_success_message(self):
         alert_product_price = self.get_alert_product_price()
         product_price = self.get_product_price()
         assert alert_product_price == product_price, \
-            f"{product_price} is not presented in success message: {alert_product_price} instead"
+            f'{product_price} is not presented in success message: {alert_product_price} instead'
 
     def should_be_success_message(self):
         self.should_be_product_name_in_success_message()
@@ -39,8 +39,8 @@ class ProductPage(BasePage):
 
     def should_not_be_success_message(self):
         assert self.is_not_element_present(self.locators.SUCCESS_MESSAGE), \
-            "Success message is presented, but should not be"
+            'Success message is presented, but should not be'
 
     def should_not_be_success_message_with_disappeared(self):
         assert self.is_disappeared(self.locators.SUCCESS_MESSAGE), \
-            "Success message did not disappear but would be"
+            'Success message is not disappear but should be'
