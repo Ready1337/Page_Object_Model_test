d3c934cc012a45f564d382015e164556b81e6c01
Ready1337 <egor.zinovjev2013@yandex.ru>
2022-03-21 01:57:56 +0500

    new locators added to BasePageLocators and LoginPageLocators

--- pages/locators.py
+++ pages/locators.py
@@ -4,6 +4,9 @@ from selenium.webdriver.common.by import By
 class BasePageLocators:
     LOGIN_LINK = (By.CSS_SELECTOR, '#login_link')
     LOGIN_LINK_INVALID = (By.CSS_SELECTOR, '#login_link_inc')
+
+    USER_ICON = (By.CSS_SELECTOR, ".icon-user")
+
     BASKET_LINK = (By.XPATH, '//span/a[contains(@href, "basket")]')
 
 
@@ -14,14 +17,18 @@ class LoginPageLocators(BasePageLocators):
     REGISTRATION_EMAIL_ADDRESS_FIELD = (By.NAME, 'registration-email')
     REGISTRATION_PASSWORD_FIELD_1 = (By.NAME, 'registration-password1')
     REGISTRATION_PASSWORD_FIELD_2 = (By.NAME, 'registration-password2')
+    REGISTER_BUTTON = (By.NAME, 'registration_submit')
 
 
 class ProductPageLocators(BasePageLocators):
     ADD_TO_BASKET_BUTTON = (By.CSS_SELECTOR, '.btn-add-to-basket')
+
     ALERT_PRODUCT_NAME = (By.XPATH, '//div[text()[contains(., "has been added to your basket")]]/strong')
     ALERT_PRODUCT_PRICE = (By.XPATH, '//p[text()[contains(., "Your basket total is now")]]/strong')
+
     PRODUCT_NAME = (By.XPATH, '//div[contains(@class, "product_main")]/h1')
     PRODUCT_PRICE = (By.XPATH, '//div[contains(@class, "product_main")]/p[contains(@class, "price_color")]')
+
     SUCCESS_MESSAGE = (By.XPATH, '//div[contains(@id, "messages")]/div')
 
 
