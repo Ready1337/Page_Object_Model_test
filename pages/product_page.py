433809c3633370303053cec4f5ebd37230d07f3e
Ready1337 <egor.zinovjev2013@yandex.ru>
2022-03-20 18:20:00 +0500

    added methods for negative tests

--- pages/product_page.py
+++ pages/product_page.py
@@ -12,17 +12,35 @@ class ProductPage(BasePage):
     def get_product_name(self):
         return self.browser.find_element(*self.locators.PRODUCT_NAME).text
 
+    def get_alert_product_name(self):
+        return self.browser.find_element(*self.locators.ALERT_PRODUCT_NAME).text
+
     def get_product_price(self):
         return self.browser.find_element(*self.locators.PRODUCT_PRICE).text
 
+    def get_alert_product_price(self):
+        return self.browser.find_element(*self.locators.ALERT_PRODUCT_PRICE).text
+
     def should_be_product_name_in_success_message(self):
-        product_name = self.browser.find_element(*self.locators.ALERT_PRODUCT_NAME).text
-        assert product_name == self.get_product_name()
+        alert_product_name = self.get_alert_product_name()
+        product_name = self.get_product_name()
+        assert alert_product_name == product_name, \
+            f"{product_name} is not presented in success message: {alert_product_name} instead"
 
     def should_be_product_price_in_success_message(self):
-        product_price = self.browser.find_element(*self.locators.ALERT_PRODUCT_PRICE).text
-        assert product_price == self.get_product_price()
+        alert_product_price = self.get_alert_product_price()
+        product_price = self.get_product_price()
+        assert alert_product_price == product_price, \
+            f"{product_price} is not presented in success message: {alert_product_price} instead"
 
     def should_be_success_message(self):
         self.should_be_product_name_in_success_message()
         self.should_be_product_price_in_success_message()
+
+    def should_not_be_success_message(self):
+        assert self.is_not_element_present(self.locators.SUCCESS_MESSAGE), \
+            "Success message is presented, but should not be"
+
+    def should_not_be_success_message_with_disappeared(self):
+        assert self.is_disappeared(self.locators.SUCCESS_MESSAGE), \
+            "Success message did not disappear but would be"
