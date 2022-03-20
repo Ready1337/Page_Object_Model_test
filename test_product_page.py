b71104605736e0bc0d2f85a9474eb92bf2939339
Ready1337 <egor.zinovjev2013@yandex.ru>
2022-03-20 23:17:53 +0500

    added test for basket page availability from product page

--- test_product_page.py
+++ test_product_page.py
@@ -2,6 +2,7 @@ import pytest
 
 from pages.product_page import ProductPage
 from pages.login_page import LoginPage
+from pages.basket_page import BasketPage
 
 
 @pytest.mark.skip('skip')
@@ -46,17 +47,28 @@ def test_message_disappeared_after_adding_product_to_basket(browser):
     product_page.should_not_be_success_message_with_disappeared()
 
 
+@pytest.mark.skip('skip')
 def test_guest_should_see_login_link_on_product_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+    link = 'http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/'
     product_page = ProductPage(browser, link)
     product_page.open()
     product_page.should_be_login_link()
 
 
+@pytest.mark.skip('skip')
 def test_guest_can_go_to_login_page_from_product_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
+    link = 'http://selenium1py.pythonanywhere.com/ru/catalogue/coders-at-work_207/'
     product_page = ProductPage(browser, link)
     product_page.open()
     product_page.go_to_login_page()
     login_page = LoginPage(browser, browser.current_url)
     login_page.should_be_login_page()
+
+
+def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
+    link = 'http://selenium1py.pythonanywhere.com/ru/catalogue/coders-at-work_207/'
+    products_page = ProductPage(browser, link)
+    products_page.open()
+    products_page.go_to_basket_page()
+    basket_page = BasketPage(browser, browser.current_url)
+    basket_page.should_be_empty_basket()
